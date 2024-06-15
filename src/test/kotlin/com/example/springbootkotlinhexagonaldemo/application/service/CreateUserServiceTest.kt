54b8889edaa1ef75510490ce618db20334c98f6f
Raynor <jonggu.woo@fitpet.co.kr>
2024-06-15 17:48:42 +0900

    🎨 Refactor

--- src/test/kotlin/com/example/springbootkotlinhexagonaldemo/application/service/CreateUserServiceTest.kt
+++ src/test/kotlin/com/example/springbootkotlinhexagonaldemo/application/service/CreateUserServiceTest.kt
@@ -7,61 +7,54 @@ import com.example.springbootkotlinhexagonaldemo.domain.entity.User
 import com.example.springbootkotlinhexagonaldemo.domain.type.common.Email
 import com.example.springbootkotlinhexagonaldemo.domain.type.personal.UserName
 import io.kotest.assertions.throwables.shouldThrow
-import io.kotest.core.spec.style.FunSpec
+import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.equality.shouldBeEqualToIgnoringFields
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class CreateUserServiceTest(
     private val createUserService: CreateUserService,
-) : FunSpec({
+) : BehaviorSpec({
 
-    test("유저를 생성할 수 있어야 한다.") {
-        // given
+    given("유저 생성 정보로") {
         val command = CreateUserUseCase.Command(
             email = Email("JhZpR-1@example.com"),
             name = UserName("홍길동-1"),
         )
 
-        val expectedUser = User(
-            email = command.email,
-            name = command.name,
-        )
-        val expectedMileage = Mileage()
+        `when`("유저 생성시") {
+            val createdUser = createUserService.createUser(command)
 
-        // when
-        val createdUser = createUserService.createUser(command)
+            then("유저 생성 정보를 토대로 유저가 생성 되어야 한다.") {
+                val expectedUser = User(
+                    email = command.email,
+                    name = command.name,
+                )
+                val expectedMileage = Mileage()
 
-        // then
-        createdUser.shouldBeEqualToIgnoringFields(
-            expectedUser,
-            User::id,
-            User::audit,
-            User::mileage,
-            User::mileageHistories,
-        )
-        createdUser.mileage.shouldBeEqualToIgnoringFields(
-            expectedMileage,
-            Mileage::id,
-        )
-    }
+                createdUser.shouldBeEqualToIgnoringFields(
+                    expectedUser,
+                    User::id,
+                    User::audit,
+                    User::mileage,
+                    User::mileageHistories,
+                )
+                createdUser.mileage.shouldBeEqualToIgnoringFields(
+                    expectedMileage,
+                    Mileage::id,
+                )
+            }
 
-    test("존재하는 이메일이라면 에러가 발생해야 한다.") {
-        // given
-        val command1 = CreateUserUseCase.Command(
-            email = Email("customer_2@dev.com"),
-            name = UserName("홍길동"),
-        )
-        val command2 = CreateUserUseCase.Command(
-            email = command1.email,
-            name = UserName("고길동"),
-        )
+            then("존재하는 이메일이라면 에러가 발생해야 한다.") {
+                val command2 = CreateUserUseCase.Command(
+                    email = command.email,
+                    name = UserName("고길동"),
+                )
 
-        createUserService.createUser(command1)
-
-        // when ~ then
-        shouldThrow<IllegalArgumentException> {
-            createUserService.createUser(command2)
+                shouldThrow<IllegalArgumentException> {
+                    createUserService.createUser(command2)
+                }
+            }
         }
     }
 })
