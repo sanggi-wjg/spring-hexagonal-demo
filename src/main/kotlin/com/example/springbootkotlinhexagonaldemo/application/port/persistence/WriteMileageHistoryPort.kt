edc64a344ff8e2d3d6115322f0c10d6b4850615d
Raynor <jonggu.woo@fitpet.co.kr>
2024-06-17 15:35:55 +0900

    🎨 코드 정리

--- src/main/kotlin/com/example/springbootkotlinhexagonaldemo/application/port/persistence/WriteMileageHistoryPort.kt
+++ src/main/kotlin/com/example/springbootkotlinhexagonaldemo/application/port/persistence/WriteMileageHistoryPort.kt
@@ -1,6 +1,5 @@
 package com.example.springbootkotlinhexagonaldemo.application.port.persistence
 
-import com.example.springbootkotlinhexagonaldemo.domain.entity.Mileage
 import com.example.springbootkotlinhexagonaldemo.domain.entity.MileageHistory
 
 interface WriteMileageHistoryPort {
